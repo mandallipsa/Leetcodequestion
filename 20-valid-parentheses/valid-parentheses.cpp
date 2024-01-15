e79843bcf96b78a6a44d6d62c96a50fcc5f6f840
mandallipsa <141558682+mandallipsa@users.noreply.github.com>
2024-01-15 14:34:18 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (38.71%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -5,22 +5,26 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> bracketStack;
-        unordered_map<char, char> bracketPairs = {{')', '('}, {']', '['}, {'}', '{'}};
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
 
-        for (char ch : s) {
-            if (bracketPairs.count(ch)) {
-                // Current character is a closing bracket
-                if (bracketStack.empty() || bracketStack.top() != bracketPairs[ch]) {
-                    return false;  // Mismatched brackets
-                }
-                bracketStack.pop();
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
             } else {
-                // Current character is an opening bracket
-                bracketStack.push(ch);
+                if (!st.empty()) {
+                    char topch = st.top();
+                    if ((ch == ')' && topch == '(') || (ch == '}' && topch == '{') || (ch == ']' && topch == '[')) {
+                        st.pop();
+                    } else {
+                        return false;  // Mismatched brackets
+                    }
+                } else {
+                    return false;  // Closing bracket with no matching opening bracket
+                }
             }
         }
 
-        return bracketStack.empty();  // If the stack is empty, all brackets are matched
+        return st.empty();  // If the stack is empty, all brackets are matched
     }
 };
