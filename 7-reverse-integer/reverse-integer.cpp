07f376afedf427977e6cc93defd8206fd343f9b3
mandallipsa <141558682+mandallipsa@users.noreply.github.com>
2024-01-21 12:25:51 +0530

    Time: 6 ms (15.89%) | Memory: 7.5 MB (6.92%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+     long ans = 0;
+        while(x){
+            int rem = x%10;
+            ans = ans*10+rem;
+            x=x/10;
+        }   
+        if(ans <INT_MIN || ans>INT_MAX){
+            return 0;
+        }
+        else{
+            return ans;
+        }
+    }
+};
