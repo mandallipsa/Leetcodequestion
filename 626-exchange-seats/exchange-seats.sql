6ba79e987642cb0508e4f34c6531d8bac850d200
mandallipsa <141558682+mandallipsa@users.noreply.github.com>
2024-02-07 15:37:00 +0530

    Time: 700 ms (44.69%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+ CASE
+    WHEN ID=(SELECT MAX(ID) FROM SEAT)AND ID%2=1
+        THEN ID
+    WHEN ID%2=1
+        THEN ID+1
+    ELSE ID-1
+END AS ID,STUDENT
+FROM SEAT 
+ORDER BY ID;
