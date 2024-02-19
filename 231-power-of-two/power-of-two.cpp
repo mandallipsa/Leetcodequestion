32dcf0be1580fe86181a5c29d184205ed4d218fa
mandallipsa <141558682+mandallipsa@users.noreply.github.com>
2024-02-19 11:10:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (39.77%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -4,13 +4,10 @@ public:
         if(n<=0)
         return false;
         while(n>1){
-            if(n%2!=0){
+            if(n%2!=0)
                 return false;
-            }
             else
-            {
                 n/=2;
-            }
         }
         return true;
     }
