0dcd72c6879497d3cb5d58206405e32a6d36dbc1
mandallipsa <141558682+mandallipsa@users.noreply.github.com>
2024-07-19 11:17:08 +0530

    Time: 4 ms (90.12%) | Memory: 8.8 MB (46.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int freqtable[256]={0};
-        for(int i=0;i<s.size();i++)
-        {
-            freqtable[s[i]]++;
-        }//O(n)
-        for(int i=0;i<t.size();i++)
-        {
-            freqtable[t[i]]--;
-        }//O(m)
-        for(int i=0;i<256;i++)
-        {
-            if(freqtable[i]!=0)
-            {
-                return false;
-            }
-        }//O(256)
-        return true;
+    int freqTable[256]={0};
+    for(int i =0;i<s.size();i++){
+        freqTable[s[i]]++;
+    }
+    for(int i =0;i<t.size();i++){
+        freqTable[t[i]]--;
+    }
+    for(int i=0;i<256;i++){
+        if(freqTable[i]!=0){
+            return false;
+        }
+    }
+  return true;
 
         
     }
