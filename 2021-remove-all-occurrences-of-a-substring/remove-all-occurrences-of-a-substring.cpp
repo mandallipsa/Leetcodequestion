257d3b88add11a2f88f3572441a39916a2df16cf
mandallipsa <141558682+mandallipsa@users.noreply.github.com>
2025-02-18 10:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (66.47%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     string removeOccurrences(string s, string part) {
     int pos = s.find(part);
-    while(pos != string::npos){
+    while(pos != string:: npos){
         s.erase(pos,part.length());
         pos = s.find(part);
-    }
-     return s;   
+        }
+    
+    return s; 
     }
 };
