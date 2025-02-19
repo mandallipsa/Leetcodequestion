5fde40f5d818631339f8c4c8a8b2652f23ba1cc3
mandallipsa <141558682+mandallipsa@users.noreply.github.com>
2025-02-19 12:41:21 +0530

    Time: 1 ms (64.92%) | Memory: 9.5 MB (90.78%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-    unordered_map<char,int> hash;
+    // unordered_map<char,int> hash;
+    // for(int i=0;i<s.length();i++){
+    //     hash[s[i]]++;
+    // }
+    // for(int i =0;i<t.length();i++){
+    //     hash[t[i]]--;
+    // }
+    // for(auto i : hash){
+    //     if(i.second !=0){
+    //         return false;
+    //     }
+    // }
+    // return true;
+    int freqTable[256]={0};
     for(int i=0;i<s.length();i++){
-        hash[s[i]]++;
+        freqTable[s[i]]++;
     }
-    for(int i =0;i<t.length();i++){
-        hash[t[i]]--;
+    for(int i=0;i<t.length();i++){
+        freqTable[t[i]]--;
     }
-    for(auto i : hash){
-        if(i.second !=0){
-            return false;
-        }
+    for(int i=0;i<256;i++){
+        if(freqTable[i]!=0)
+        return false;
     }
     return true;
     }
